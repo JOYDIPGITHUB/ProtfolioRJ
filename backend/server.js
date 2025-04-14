7eb3075aeda8e7d092668486550860febca48ef7
Joydip Mondal <joydipmondal27092002@gmail.com>
2025-04-14 19:09:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uplodes', express.static(path.join(__dirname, 'uplodes')));
 
 // DB Connection
 mongoose.connect(process.env.MONGO_URI, {
